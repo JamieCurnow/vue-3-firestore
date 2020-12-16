15a93e0b281a1dc83133de5c13ed0a85befd3b79
JamieCurnow <jamie@island-web.ca>
2020-12-16 12:32:11 +0000

    add keywords and delete dist

--- dist/types/firestoreTypes.d.ts
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-/** Firestore DocumentReference */
-export declare type Docref = firebase.firestore.DocumentReference<firebase.firestore.DocumentData>;
-/** Firestore CollectionReference */
-export declare type CollectionRef = firebase.firestore.CollectionReference<firebase.firestore.DocumentData>;
-/** Firestore Query */
-export declare type Query = firebase.firestore.Query<firebase.firestore.DocumentData>;
--- dist/types/type-gates.d.ts
+++
@@ -1,5 +0,0 @@
-import { Docref, CollectionRef } from './firestoreTypes';
-import { Options, OptionsCollWatch, OptionsCollGet, OptionsDocGet } from './Options';
-export declare const firestoreRefIsDoc: (firestoreRef: CollectionRef | Docref) => firestoreRef is Docref;
-export declare const optsAreColl: <T, M>(o: Options<T, M>) => o is OptionsCollWatch<T, M> | OptionsCollGet<T, M>;
-export declare const optsAreGet: <T, M>(o: Options<T, M>) => o is OptionsCollGet<T, M> | OptionsDocGet<T, M>;
